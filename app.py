dcb42ab797d08d859eaba7baa83ae4a561eff073
rogeazzam <roge_azzam@hotmail.com>
2024-06-01 17:35:26 +0300

    Modifying input receiving

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import random
 first_id, last_id = 1, 2159
 
 def main():
+    backend_ip = input('Please enter the backend IP: ')
+    URL = f"http://{backend_ip}:5001/mongodb"
     while True:
         draw = input("Would you like to draw a Pokemon?(yes/no): ")
         if draw != "yes":
@@ -16,14 +18,12 @@ def main():
         download_ids = random.sample(range(first_id, last_id), 1)
         downloaded_pokemons = []
         for id in download_ids:
-            downloaded_pokemons.append(Pokemon(f"https://pokeapi.co/api/v2/item/{str(id)}"))
+            downloaded_pokemons.append(Pokemon(f"http://pokeapi.co/api/v2/item/{str(id)}"))
 
         rand_ind = random.randint(0, len(downloaded_pokemons) - 1)
         selected_pokemon = downloaded_pokemons[rand_ind]
 
-        URL = "http://10.0.0.1:5001/mongodb"
         data = {
-            "url": "mongodb://localhost:27017/",  # Adjust your MongoDB URI as needed
             "database": "db_name",
             "collection": "pokemons",
             "Document": selected_pokemon.json_convertion()
@@ -31,12 +31,9 @@ def main():
         headers = {'Content-Type': 'application/json'}
 
         r = requests.get(url=URL, data=json.dumps(data), headers=headers)
-        print(r.json())
 
-        if r.json() != "Doesn't Exist":
-            print("Exists")
-        else:
-            print("Doesn't exist")
+        if r.json() == "Doesn't Exist":
+            print("Doesn't Exist")
             r = requests.post(url=URL, data=json.dumps(data), headers=headers)
         print(selected_pokemon)
 
